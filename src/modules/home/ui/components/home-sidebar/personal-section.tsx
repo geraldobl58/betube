0418852565df8bcf017bea4ffa8c12dec3615049
Geraldo Luiz <geraldobl58@gmail.com>
2025-03-10 12:47:50 -0300

    add PersonalSection component to HomeSidebar for user-specific navigation

---
+++ src/modules/home/ui/components/home-sidebar/personal-section.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import Link from 'next/link';
+
+import { HistoryIcon, ListVideoIcon, ThumbsUpIcon } from 'lucide-react';
+
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar';
+
+const items = [
+  {
+    title: 'History',
+    url: '/playlist/history',
+    icon: HistoryIcon,
+    auth: true,
+  },
+  {
+    title: 'Liked videos',
+    url: '/playlist/liked',
+    icon: ThumbsUpIcon,
+    auth: true,
+  },
+  {
+    title: 'All playlists',
+    url: '/playlists',
+    icon: ListVideoIcon,
+    auth: true,
+  },
+];
+
+export const PersonalSection = () => {
+  return (
+    <SidebarGroup>
+      <SidebarGroupLabel>You</SidebarGroupLabel>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton
+                asChild
+                isActive={false}
+                tooltip={item.title}
+                onClick={() => {}}
+              >
+                <Link href={item.url} className="flex items-center gap-4">
+                  <item.icon />
+                  <span className="text-sm">{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
+};
