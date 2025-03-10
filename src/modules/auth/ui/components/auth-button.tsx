f82885d8550de8eb50731f873dd7157d53596320
Geraldo Luiz <geraldobl58@gmail.com>
2025-03-10 15:06:28 -0300

    refactor AuthButton component to integrate Clerk authentication with SignedIn and SignedOut states

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -1,24 +1,39 @@
-import { Button } from '@/components/ui/button';
+'use client';
+
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
+
 import { UserCircleIcon } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
 
 export const AuthButton = () => {
   return (
-    <Button
-      variant="outline"
-      className="
-        px-4
-        py-2
-        font-medium
-        text-blue-600
-        hover:text-blue-500
-        border-blue-500/20
-        rounded-full
-        shadow-none
-        [&_svg]:size-5
-      "
-    >
-      <UserCircleIcon />
-      Sign In
-    </Button>
+    <>
+      <SignedIn>
+        <UserButton />
+        {/* Add menu items for studio and User Profile */}
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button
+            variant="outline"
+            className="
+            px-4
+            py-2
+            font-medium
+            text-blue-600
+            hover:text-blue-500
+            border-blue-500/20
+            rounded-full
+            shadow-none
+            [&_svg]:size-5
+          "
+          >
+            <UserCircleIcon />
+            Sign In
+          </Button>
+        </SignInButton>
+      </SignedOut>
+    </>
   );
 };
