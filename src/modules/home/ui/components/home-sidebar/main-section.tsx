2ec2eecb530b3521177076ac68c2d76ff5de099d
Geraldo Luiz <geraldobl58@gmail.com>
2025-03-10 12:47:43 -0300

    add MainSection component to HomeSidebar for navigation links

---
+++ src/modules/home/ui/components/home-sidebar/main-section.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import Link from 'next/link';
+
+import { FlameIcon, HomeIcon, PlaySquareIcon } from 'lucide-react';
+
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar';
+
+const items = [
+  {
+    title: 'Home',
+    url: '/',
+    icon: HomeIcon,
+  },
+  {
+    title: 'Subscriptions',
+    url: '/feed/subscriptions',
+    icon: PlaySquareIcon,
+    auth: true,
+  },
+  {
+    title: 'Trending',
+    url: '/feed/trending',
+    icon: FlameIcon,
+  },
+];
+
+export const MainSection = () => {
+  return (
+    <SidebarGroup>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton
+                asChild
+                isActive={false}
+                tooltip={item.title}
+                onClick={() => {}}
+              >
+                <Link href={item.url} className="flex items-center gap-4">
+                  <item.icon />
+                  <span className="text-sm">{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
+};
