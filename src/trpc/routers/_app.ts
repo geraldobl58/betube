52f40d47a671c497bd47f9b2f3ecd8c0fcea2c66
Geraldo Luiz <geraldobl58@gmail.com>
2025-03-11 14:46:14 -0300

    feat: replace baseProcedure with protectedProcedure for hello query

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,17 +1,21 @@
 import { z } from 'zod';
-import { baseProcedure, createTRPCRouter } from '../init';
+
+import { createTRPCRouter, protectedProcedure } from '../init';
+
 export const appRouter = createTRPCRouter({
-  hello: baseProcedure
+  hello: protectedProcedure
     .input(
       z.object({
         text: z.string(),
       })
     )
     .query((opts) => {
+      console.log({ dbUser: opts.ctx.user });
+
       return {
         greeting: `hello ${opts.input.text}`,
       };
     }),
 });
-// export type definition of API
+
 export type AppRouter = typeof appRouter;
